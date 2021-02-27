5fddce59fc22e4b85041c0a244a1b1af9c4e4b36
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2021-02-27 12:31:03 +0100

    Add helper functions, macros and documentation entries to definitions

--- src/main.lisp
+++ src/main.lisp
@@ -3,34 +3,55 @@
 (in-package :cl-ciph)
 
 (defun read-file (filename)
+  "Returns FILENAME contents (text) as a string"
   (with-open-file (in filename)
     (with-standard-io-syntax
       (read in))))
 
-(defparameter s "hola esto es un test")
-
 (defun str->keyword-list (s)
+  "Returns S a list of uppercase sensitive keywords"
   (loop for char across s collect (intern (string char) :keyword)))
 
-(defparameter a (str->keyword-list "hola esto es un test"))
+(defun make-keys (keywords)
+  "Removes duplicates and orders alphabetically the KEYWORDS list"
+  (sort (remove-duplicates keywords :key #'symbol-name)
+        (lambda (x y) (string< (symbol-name x) (symbol-name y)))))
 
 (defun get-mapping-plist (str)
-  (let ((ks (sort (remove-duplicates (str->keyword-list str) :key #'symbol-name)
-                  (lambda (x y) (string< (symbol-name x) (symbol-name y))))))
+  "Returns a PLIST containing a keyword->string mapping of STR characters"
+  (let ((ks (make-keys (str->keyword-list str))))
     (mapcan #'list ks (mapcar #'symbol-name ks))))
 ;; using mapcar we can generate an alist instead of a plist
 
 (defmacro mpget (mapping k)
+  "Returns MAPPING value for key K expresses as string"
   `(getf ,mapping (intern (string ,k) :keyword)))
 
-(defparameter mp (get-mapping-plist s))
-
 (defmacro update-mapping (mapping key new-value)
+  "Updates MAPPING KEY entry with NEW-VALUE"
   (let ((k (intern (string key) :keyword)))
     `(setf (getf ,mapping ,k) ,new-value)))
 
-(defun apply-mapping (mapping str) nil)
+(defun apply-mapping (mapping str)
+  "Applies a transformation based on MAPPING to STR"
+  (format nil "~{~a~}"
+          (loop for char across str collect (mpget mapping char))))
 
-(defun frequencies (str) nil)
-(defun rot (mapping shift) nil)
+(defun mycount (str elem)
+  "Counts appearances of ELEM in STR"
+  (length (loop for c across str when (eql c elem) collect c)))
+
+(defmacro symbol-char (symbol)
+  "Returns a one char SYMBOL name as a character"
+  `(char (symbol-name ,symbol) 0))
+
+(defun frequencies (str)
+  "Returns a PLIST containing the frequency distribution on STR"
+  (let* ((keywords (str->keyword-list str))
+         (ks (make-keys keywords))
+         (l (length str)))
+    (mapcan #'list ks (loop for k in ks collect (float (/ (mycount str (symbol-char k)) l))))))
+
+(defun rot (str shift) nil)
+
 (defun rot-brute-force (str) nil)
