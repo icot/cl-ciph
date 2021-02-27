6a7c5954b8ab8f2152755be14feed075f06fc6d3
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2021-02-27 11:32:52 +0100

    wip: get-mapping-plist, mpget, update-mapping

--- src/main.lisp
+++ src/main.lisp
@@ -7,17 +7,30 @@
     (with-standard-io-syntax
       (read in))))
 
-(defun str->list (s)
-  (loop for char across s collect char))
+(defparameter s "hola esto es un test")
 
-(defun get-mapping (str) nil)
+(defun str->keyword-list (s)
+  (loop for char across s collect (intern (string char) :keyword)))
 
-(defun update-mapping (mapping key value) nil)
+(defparameter a (str->keyword-list "hola esto es un test"))
+
+(defun get-mapping-plist (str)
+  (let ((ks (sort (remove-duplicates (str->keyword-list str) :key #'symbol-name)
+                  (lambda (x y) (string< (symbol-name x) (symbol-name y))))))
+    (mapcan #'list ks (mapcar #'symbol-name ks))))
+;; using mapcar we can generate an alist instead of a plist
+
+(defmacro mpget (mapping k)
+  `(getf ,mapping (intern (string ,k) :keyword)))
+
+(defparameter mp (get-mapping-plist s))
+
+(defmacro update-mapping (mapping key new-value)
+  (let ((k (intern (string key) :keyword)))
+    `(setf (getf ,mapping ,k) ,new-value)))
 
 (defun apply-mapping (mapping str) nil)
 
 (defun frequencies (str) nil)
-
 (defun rot (mapping shift) nil)
-
 (defun rot-brute-force (str) nil)
